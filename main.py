37d0b3d31741a152d295eb9947763251b59c8a6c
Ahmed Khafagy <ahmedsidki65@gmail.com>
2025-06-30 18:47:40 +0300

    تعديل تشغيل التطبيق ببورت 10000

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def download_file():
     return "❌ لا يوجد ملف للتحميل"
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-
+   if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
